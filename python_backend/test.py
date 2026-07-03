f0cc329c14fe893ab846ec57f375413555f17043
mrunalvs09 <mrunalvs09@gmail.com>
2026-07-03 19:23:55 +0530

    updated python backend

--- python_backend/test.py
+++ python_backend/test.py
@@ -1,18 +1,18 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 
-# NumPy Array
-arr = np.array([3, 20, 30, 40, 50])
-print("NumPy Array:")
-print(arr)
+# # NumPy Array
+# arr = np.array([3, 20, 30, 40, 50])
+# print("NumPy Array:")
+# print(arr)
 
-# Pandas DataFrame
-data = {
-    "Name": ["Mrunal", "hhh", "ffff"],
-    "Age": [21, 22, 20]
-}
+# # Pandas DataFrame
+# data = {
+#     "Name": ["Mrunal", "hhh", "ffff"],
+#     "Age": [21, 22, 20]
+# }
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
-print("\nPandas DataFrame:")
-print(df)
+# print("\nPandas DataFrame:")
+# print(df)
